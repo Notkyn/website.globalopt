e49f4683ca4e3013e8e85f21664214d974f510b5
notky <notky@ukr.net>
2020-05-28 12:05:47 +0300

    added button: page up

--- dist/js/script.js
+++ dist/js/script.js
@@ -17,4 +17,23 @@ $(document).ready(function(){
             }
           ]
     });
+
+    // Smooth scroll and page up
+
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 1600){
+          $('.pageup').fadeIn();
+        } else {
+          $('.pageup').fadeOut();
+        }
+      });
+  
+      $("a[href='#up']").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+      });
+
   });
+
+  
