f0d693f13b5857c0238edabe07cbc75d64f42573
notky <notky@ukr.net>
2020-05-28 11:24:31 +0300

    added slider for reviews

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
-    $('.slider').slick();
+    
+    // Slider
+
+    $('.slider').slick({
+        centerMode: true,
+        centerPadding: '0px',
+        slidesToShow: 3,
+        prevArrow: '<button type="button" class="slick-prev"><img src="./icons/arrow.svg" alt="preious"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="./icons/arrow.svg" alt="next"></button>',
+        responsive: [
+            {
+              breakpoint: 1200,
+              settings: {
+                slidesToShow: 1
+              }
+            }
+          ]
+    });
   });
